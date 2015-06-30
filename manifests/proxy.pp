8f89c91822222652b709bdb9b496d776dfbc12ff
Sam Morrison <sorrison@gmail.com>
2015-06-30 12:17:59 +1000

    Support multiple backends
    Change-Id: I70f43e8ea57812978bed6487a21280cce399fec6

---
+++ manifests/proxy.pp
@@ -0,0 +1,41 @@
+# Proxy defined type
+define pound::proxy($port, $backend_port, $ssl=true, $backend_ip=false, $backends=undef, $emergency_ip=false, $emergency_port=false, $nagios_check=true) {
+
+  include pound
+
+  if $backend_ip {
+    $backends_real = [$backend_ip,]
+  } else {
+    $backends_real = $backends
+  }
+  if $ssl {
+    $listen_protocol = 'ListenHTTPS'
+
+    File <| tag == 'sslcert' |> {
+      notify +> Service['pound'],
+    }
+
+  }
+  else {
+    $listen_protocol = 'ListenHTTP'
+  }
+
+  concat::fragment {"vhost-${name}":
+    target  => $pound::pound_cfg,
+    content => template('pound/vhost.cfg.erb'),
+    order   => 10,
+  }
+
+  if $nagios_check {
+    if $ssl {
+      nagios::service { "http_${port}":
+        check_command => "https_port!${port}";
+      }
+    }
+    else {
+      nagios::service { "http_${port}":
+        check_command => "http_port!${port}";
+      }
+    }
+  }
+}
